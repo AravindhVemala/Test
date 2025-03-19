4d3bfe9e78f11288f91f6c9cb8232055ffe327a4
aravindh vemala <v.aravindh2406@gmail.com>
2025-03-19 16:31:42 +0530

    Updated backend for updates

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,3 +1,17 @@
+// router.get("/", async (req, res) => {
+//     const userId = req.query.userId; // Get user ID from request
+
+//     if (!userId) {
+//         return res.status(400).json({ error: "User ID is required" });
+//     }
+
+//     try {
+//         const tasks = await Task.find({ userId }); // Fetch tasks for this user
+//         res.json(tasks);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 
 app.get("/tasks", async (req, res) => {
     try {
@@ -11,6 +25,21 @@ app.get("/tasks", async (req, res) => {
     }
 });
 
+// router.post("/", async (req, res) => {
+//     const { text, userId } = req.body;
+
+//     if (!userId) {
+//         return res.status(400).json({ error: "User ID is required" });
+//     }
+
+//     try {
+//         const newTask = new Task({ text, userId });
+//         await newTask.save();
+//         res.json(newTask);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
 
 app.post("/tasks", async (req, res) => {
     try {
